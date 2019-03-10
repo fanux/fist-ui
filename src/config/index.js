dbb9a1e0f0a15cce9a9de1f7ef52201ce8474a6d
cuisongliu <cuisongliu@qq.com>
2019-03-10 11:54:27 +0800

    fix iview admin for fist ui

--- src/config/index.js
+++ src/config/index.js
@@ -17,8 +17,8 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://localhost:8080',
-    pro: 'http://fist'
+    dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
+    pro: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/'
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
