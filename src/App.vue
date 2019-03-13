4e95803dbfc4b8ac0b4a379c2040869a2e6f0cd8
cuisongliu <cuisongliu@qq.com>
2019-03-13 15:44:07 +0800

    fix lose

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
   <div id="app">
-    <router-view/>
+    <login></login>
   </div>
 </template>
 
 <script>
+import login from './components/login'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'login': login
+  }
 }
 </script>
 
-<style lang="less">
-.size{
-  width: 100%;
-  height: 100%;
-}
-html,body{
-  .size;
-  overflow: hidden;
-  margin: 0;
-  padding: 0;
-}
-#app {
-  .size;
-}
+<style>
 </style>
